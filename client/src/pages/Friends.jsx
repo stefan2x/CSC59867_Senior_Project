b08f5ba54cb088ea9b7f917026ebd672f926bfec
Matheus Figueroa <mfigueroa1327@gmail.com>
2023-05-16 01:04:55 -0400

    friend page added

--- client/src/pages/Friends.jsx
+++ client/src/pages/Friends.jsx
@@ -2,8 +2,67 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { publicRequest } from "../requestMethods";
 import Navbar from "../components/Navbar";
+import styled from 'styled-components'
+import { mobile } from "../responsive";
+import { Avatar } from "@material-ui/core";
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline';
 
+const FriendContainer = styled.div`
+  padding: 20px;
+  ${mobile({ padding: "10px" })}
+`;
+const Friend = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
 
+const FriendDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const WishDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WishContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ViewWishes = styled.div`
+  font-size: 14px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+`;
+
+const AddFriend = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
+`;
 
 const Friends = () =>{
     const [users,setUsers] = useState([])
@@ -25,9 +84,36 @@ const Friends = () =>{
     return (
         <>
             <Navbar/>
-            {users.map(user => (
-                <div>{user.firstname}</div>
-            ))}
+            <FriendContainer>
+                {users.map(user => (
+                    <Friend>
+                    <FriendDetail>
+                    {/*<Image src={product.img} />*/}
+                    <Avatar/>
+                    <Details>
+                        <div>
+                        <b>Product:</b> {user.firstname}
+                        </div>
+                        {/*<ProductColor color={product.color} />*/}
+                        <div>
+                        <b>Size:</b> {user.lastname}
+                        </div>
+                    </Details>
+                    </FriendDetail>
+                    <WishDetail>
+                    <WishContainer>
+                        <ViewWishes>
+                            View wishlist
+                            <ViewHeadlineIcon/>
+                        </ViewWishes>
+                    </WishContainer>
+                    <AddFriend>
+                        <PersonAddIcon/>
+                    </AddFriend>
+                    </WishDetail>
+                </Friend>
+                ))}
+            </FriendContainer>
         </>
     )
 
